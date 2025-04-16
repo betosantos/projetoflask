f14669f6669fb16e208f24aea8e488db5ea52beb
betosantos <oliveira.roberto@gmail.com>
2025-04-16 14:50:53 -0300

    Railway string de conexao

--- app.py
+++ app.py
@@ -4,54 +4,43 @@ from datetime import datetime
 from models import db, Pessoa
 from dotenv import load_dotenv
 
-
-# Carrega variáveis de ambiente
 load_dotenv()
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sua_chave_secreta'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:caconde138@localhost:3306/flask'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'chave_padrao')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicializa banco
 db.init_app(app)
-
 
 @app.route('/')
 def home():    
     return render_template('index.html')
-
 
 @app.route('/pessoas')
 def index():    
     pessoas = Pessoa.query.all()    
     return render_template('index.html', pessoas=pessoas)
 
-
 @app.route('/form')
 def form():
     pessoas = Pessoa.query.all()    
     return render_template('form.html', pessoas=pessoas)
-
 
 @app.route('/add', methods=['POST'])
 def add():
     nome = request.form['nome']
     email = request.form['email']
 
-    nova_pessoa = Pessoa(
-        nome=nome, 
-        email=email
-    )
+    nova_pessoa = Pessoa(nome=nome, email=email)
     db.session.add(nova_pessoa)
     db.session.commit()
 
     return redirect(url_for('form'))
 
-
-
-
+# Criação das tabelas se ainda não existirem
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
