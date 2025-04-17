6d551b3a98fc9515aae1e2247a8b9704877901f5
betosantos <oliveira.roberto@gmail.com>
2025-04-17 10:14:18 -0300

    Railway Banco de dados com formulario de cadastro

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from db import db
 from models import Usuario
-
 load_dotenv()
 
 app = Flask(__name__)
@@ -12,21 +11,32 @@ app = Flask(__name__)
 # Configurações do banco
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:AjdObfnXSVPSnIzUmZYpaUCLkPXKZUTr@tramway.proxy.rlwy.net:55888/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 # Inicializa o db com o app
 db.init_app(app)
-
 # Cria as tabelas
 with app.app_context():
     db.create_all()
+
+
 
 @app.route('/')
 def home():    
     return render_template('index.html')
 
-@app.route('/form')
-def form(): 
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        novo_usuario = Usuario(nome=nome, email=email)
+        db.session.add(novo_usuario)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('form.html')
+
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from db import db
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    
 
     def __repr__(self):
         return f'<Usuario {self.nome}>'
