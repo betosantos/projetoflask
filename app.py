7f6ecb15660641fc2f3b2198e3205e5b2082955b
betosantos <oliveira.roberto@gmail.com>
2025-04-16 23:35:28 -0300

    Railway debug string

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'chave_padrao')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:AjdObfnXSVPSnIzUmZYpaUCLkPXKZUTr@tramway.proxy.rlwy.net:55888/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 print("App:", app)
