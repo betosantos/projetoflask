6db5ff2b20a501f6ee7f90ef632a904ee27de344
betosantos <oliveira.roberto@gmail.com>
2025-04-16 23:09:53 -0300

    Railway config db

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'chave_padrao')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
 
 
 @app.route('/')
