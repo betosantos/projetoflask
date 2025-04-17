dac78efd91edf71d2f360c1b4cdca84ca6413e76
betosantos <oliveira.roberto@gmail.com>
2025-04-17 09:19:57 -0300

    Railway flask basico

--- app.py
+++ app.py
@@ -6,44 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'chave_padrao')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:AjdObfnXSVPSnIzUmZYpaUCLkPXKZUTr@tramway.proxy.rlwy.net:55888/railway'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-print("App:", app)
-print("DB:", db)
-
-db.init_app(app)  # <-- aqui dá erro se importar Pessoa antes disso
-
-from models import Pessoa  # <-- só importa depois de db.init_app(app)
 
 
 @app.route('/')
 def home():    
     return render_template('index.html')
 
-@app.route('/pessoas')
-def index():    
-    pessoas = Pessoa.query.all()
-    return render_template('index.html', pessoas=pessoas)
 
 @app.route('/form')
-def form():
-    pessoas = Pessoa.query.all()    
-    return render_template('form.html', pessoas=pessoas)
+def form(): 
+    return render_template('form.html')
 
-@app.route('/add', methods=['POST'])
-def add():
-    nome = request.form['nome']
-    email = request.form['email']
-    nova_pessoa = Pessoa(nome=nome, email=email)
-    db.session.add(nova_pessoa)
-    db.session.commit()
-    return redirect(url_for('form'))
 
-# Criar tabelas ao subir
-with app.app_context():
-    db.create_all()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
