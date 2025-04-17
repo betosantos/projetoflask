58e9f0ce309a10d26681b37563519718eaae10ee
betosantos <oliveira.roberto@gmail.com>
2025-04-17 10:05:26 -0300

    Railway com dados

--- app.py
+++ app.py
@@ -2,29 +2,31 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from db import db
+from models import Usuario
 
 load_dotenv()
 
 app = Flask(__name__)
 
-# Configuração do banco de dados MySQL
+# Configurações do banco
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:AjdObfnXSVPSnIzUmZYpaUCLkPXKZUTr@tramway.proxy.rlwy.net:55888/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Inicializa o db com o app
+db.init_app(app)
 
-db = SQLAlchemy(app)
-print(db)
+# Cria as tabelas
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():    
     return render_template('index.html')
 
-
 @app.route('/form')
 def form(): 
     return render_template('form.html')
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 
 from db import db
 
-class Pessoa(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<Pessoa {self.nome}>'
+        return f'<Usuario {self.nome}>'
