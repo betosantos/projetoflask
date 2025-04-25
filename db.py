6a9bfe37ee0b6dcd390edad8906b96d432031ce1
betosantos <oliveira.roberto@gmail.com>
2025-04-25 09:53:39 -0300

    alteração init_db para app.py

--- db.py
+++ db.py
@@ -8,6 +8,6 @@ load_dotenv()
 db = SQLAlchemy()
 
 def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:AjdObfnXSVPSnIzUmZYpaUCLkPXKZUTr@tramway.proxy.rlwy.net:55888/railway'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
